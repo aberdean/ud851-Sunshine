62ba37d35d5b74aaad81e81f1bb539e2f055495c
Antonella Bernobich Dean <aberdean@users.noreply.github.com>
2017-01-12 13:30:37 +0100

    Solve add weather list

---
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/WeatherData.java
@@ -0,0 +1,70 @@
+package com.example.android.sunshine;
+
+/**
+ * Created by aberdean on 1/12/2017.
+ */
+
+public final class WeatherData {
+    /**
+     * This method returns a list of fake weather data.
+     *
+     * @return A list of weather data
+     */
+    public static String[] getWeatherData() {
+        return new String[]{
+                "Storm",
+                "Drizzle",
+                "Light Rain",
+                "Moderate Rain",
+                "Heavy Rain",
+                "Intense Rain",
+                "Extreme Rain",
+                "Freezing Rain",
+                "Light Shower",
+                "Shower",
+                "Heavy Shower",
+                "Ragged Shower",
+                "Light Snow",
+                "Snow",
+                "Heavy Snow",
+                "Sleet",
+                "Shower Sleet",
+                "Rain and Snow",
+                "Shower Snow",
+                "Mist",
+                "Smoke",
+                "Haze",
+                "Sand, Dust",
+                "Fog",
+                "Sand",
+                "Dust",
+                "Volcanic Ash",
+                "Squalls",
+                "Tornado",
+                "Clear",
+                "Mostly Clear",
+                "Scattered Clouds",
+                "Broken Clouds",
+                "Overcast Clouds",
+                "Tornado",
+                "Tropical Storm",
+                "Hurricane",
+                "Cold",
+                "Hot",
+                "Windy",
+                "Hail",
+                "Calm",
+                "Light Breeze",
+                "Gentle Breeze",
+                "Breeze",
+                "Fresh Breeze",
+                "Strong Breeze",
+                "High Wind",
+                "Gale",
+                "Severe Gale",
+                "Storm",
+                "Violent Storm",
+                "Hurricane"
+        };
+    }
+}
